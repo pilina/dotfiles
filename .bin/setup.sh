14b12cf845ff09bae8903d80bca3ed0f4efa7ac5
Thomas Stachl <thomas@stachl.me>
2021-11-21 13:01:30 -0800

    function call ran loop

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -14,7 +14,7 @@ main() {
   dependencies
   harden
   if [ ! -f /.dockerenv ]; then
-    docker
+    setup_docker
   fi
   user
 }
@@ -51,13 +51,13 @@ harden() {
   ufw enable
 }
 
-docker() {
+setup_docker() {
   # add docker gpg key
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   # add docker repository
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   # install docker engine
   apt update && apt -y install docker-ce docker-ce-cli containerd.io
   # init docker swarm
